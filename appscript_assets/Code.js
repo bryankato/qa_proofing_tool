2c6f9767b3ba9b4c51838dafcdcfafa10f6631eb
Bryan Kato <bkato@google.com>
2017-11-29 10:09:48 +0000

    fixed bug with non secure http links not being detected as tracked

--- appscript_assets/Code.js
+++ appscript_assets/Code.js
@@ -91,7 +91,8 @@ function getTrackedData(url) {
       "url" : url,
       "label" : "optout"
     };
-  } else if (url.indexOf("https://www.google.com/appserve/mkt/proof") == 0) {
+  } else if (url.indexOf("https://www.google.com/appserve/mkt/proof") == 0 ||
+             url.indexOf("http://www.google.com/appserve/mkt/proof") == 0) {
   // In case of normal tracked link
     // Fetch content from URL
     var htmlString = UrlFetchApp.fetch(url).getContentText();
